538d9d4e6f164c0a592c8339d3d329c186a1d5af
Marchino13 <1261302911@qq.com>
2024-05-11 13:01:15 +0800

    lxd新建maven测试

---
+++ lxd-test/src/test/java/com/example/lxd/LxdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lxd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LxdTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
