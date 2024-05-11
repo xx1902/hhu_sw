5ee43e0509554b7c04c5fe04a4fc91c5cf51b94e
Marchino13 <1261302911@qq.com>
2024-05-11 13:11:49 +0800

    我是不是提交到了main分支?

---
+++ lxd-test/src/test/java/com/example/lxdtest/LxdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lxdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LxdTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
