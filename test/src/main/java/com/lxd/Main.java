90586f0fc606f275cc76ce2c51be6eb9e146fe87
Marchino13 <1261302911@qq.com>
2024-05-11 15:08:39 +0800

    lxd测试1

---
+++ test/src/main/java/com/lxd/Main.java
@@ -0,0 +1,13 @@
+package com.lxd;
+
+
+/**
+ * @author Marchino
+ * @date 2024/5/11 15:07
+ * @description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
