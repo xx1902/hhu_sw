ccb45d62bf472760f071aa86876031aa644e45c7
Marchino13 <1261302911@qq.com>
2024-05-11 13:47:45 +0800

    lxd的test

---
+++ src/main/java/com/lxd/Main.java
@@ -0,0 +1,14 @@
+package com.lxd;
+
+
+/**
+ *
+ * @author Marchino
+ * @date 2024/5/11 13:47
+ * @description 
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
