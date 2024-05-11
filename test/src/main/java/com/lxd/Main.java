04e4d2736e8c0228f2c9cee7fe8c9d9a7083101b
小星 <1736357992@qq.com>
2024-05-11 15:42:51 +0800

    lxd的test

--- test/src/main/java/com/lxd/Main.java
+++ test/src/main/java/com/lxd/Main.java
@@ -9,5 +9,6 @@ package com.lxd;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("你好lxd qxk sb");
     }
 }
