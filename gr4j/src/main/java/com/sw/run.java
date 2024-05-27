1f3c030307efc57ecf2a3b2415c723d97332d623
Marchino13 <1261302911@qq.com>
2024-05-27 15:07:29 +0800

    读取模型参数

---
+++ gr4j/src/main/java/com/sw/run.java
@@ -0,0 +1,37 @@
+package com.sw;
+
+
+import com.sw.unit.DataReader;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * @author Marchino
+ * @date 2024/5/27 14:09
+ * @description 读取流域径流数据，流域参数数据， 模型初始状态量数据
+ * @version 1.0
+ */
+
+public class run {
+    public static void main(String[] args) {
+        //加载模型参数, 第一列逐日降雨量， 第二列
+        List<String[]> paras = DataReader.lowAndRowRead("./inputData.txt");
+
+        //加载gr4j模型的状态变量和流域大小
+        List<Double> otherParas = DataReader.lowRead("./others.txt");
+        double area = otherParas.get(0);//测试流域面积的大小（km2）
+        double upperTankRadio = otherParas.get(1);//上层产流水库初始填充
+        double lowerTankRadio = otherParas.get(2);//下层产流水库初始填充
+
+        //TODO gr4j_Parameter参数读入
+
+        //test
+        for (String[] para : paras) {
+            for (String s : para) {
+                System.out.print(s + "\t\t");
+            }
+            System.out.println(" ");
+        }
+    }
+}
---
+++ gr4j/src/main/java/com/sw/unit/DataReader.java
@@ -0,0 +1,49 @@
+package com.sw.unit;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataReader {
+
+    /***
+     * @description 读取参数
+     * @param: filePath
+     * @return java.util.List<java.lang.String [ ]>
+     * @author Marchino
+     * @date 14:17 2024/5/27
+     */
+    public static List<String[]> lowAndRowRead(String filePath) {
+        List<String[]> data = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split("\t");
+                data.add(values);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+
+    public static List<Double> lowRead(String filePath) {
+        List<Double> data = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                double value = Double.parseDouble(line);
+                data.add(value);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+}
