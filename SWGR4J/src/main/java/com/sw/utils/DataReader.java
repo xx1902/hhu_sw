767aaa2df8f7c3f68e846fda15a3a07c9d8d56d6
Marchino13 <1261302911@qq.com>
2024-06-15 22:54:23 +0800

    参数读取工具类编写

--- SWGR4J/src/main/java/com/sw/utils/DataReader.java
+++ SWGR4J/src/main/java/com/sw/utils/DataReader.java
@@ -1,5 +1,11 @@
 package com.sw.utils;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Marchino
  * @date 2024/6/15 22:34
@@ -7,4 +13,52 @@ package com.sw.utils;
  */
 
 public class DataReader {
+    /***
+     * @description 读取参数格式如:   1.5	6.5	0.9393
+     * @param: filePath
+     * @return java.util.List<java.lang.String[]>
+     * @author Marchino
+     * @date 22:51 2024/6/15
+     */
+    public static List<String[]> colAndRowRead(String filePath) {
+        List<String[]> data = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split("\t");
+                data.add(values);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+
+    /***
+     * @description 读取参数, 格式如
+     *                      120.5
+     *                      0.6
+     *                      0.7
+     * @param: filePath
+     * @return java.util.List<java.lang.Double>
+     * @author Marchino
+     * @date 22:53 2024/6/15
+     */
+    public static List<Double> colRead(String filePath) {
+        List<Double> data = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                double value = Double.parseDouble(line);
+                data.add(value);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
 }
