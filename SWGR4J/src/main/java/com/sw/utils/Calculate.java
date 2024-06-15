9e800fb8b2cdf1457e2037ad1715248bb32714d1
QinXingkang <2505083096@qq.com>
2024-06-16 00:08:17 +0800

    完成CalPs和CalEs

--- SWGR4J/src/main/java/com/sw/utils/Calculate.java
+++ SWGR4J/src/main/java/com/sw/utils/Calculate.java
@@ -1,6 +1,5 @@
 package com.sw.utils;
 
-import com.sw.unit.Cal;
 import com.sw.unit.DataReader;
 
 import java.util.ArrayList;
@@ -14,34 +13,34 @@ import java.util.List;
 
 public class Calculate {
 
-    public double[][] getPnAndEn(){
-        //加载gr4j模型的状态变量和流域大小
-        List<Double> otherParas = com.sw.unit.DataReader.colRead("./others.txt");
-        double area = otherParas.get(0);//测试流域面积的大小（km2）
-        double upperTankRadio = otherParas.get(1);//上层产流水库初始填充
-        double lowerTankRadio = otherParas.get(2);//下层产流水库初始填充
-
-        //gr4j_Parameter参数读入
-        List<Double> para = com.sw.unit.DataReader.colRead("./GR4J_Parameter.txt");
-        double x1 = para.get(0);//产流水库容量
-        double x2 = para.get(1);//地下水交换系数
-        double x3 = para.get(2);//流域水库容量
-        double x4 = para.get(3);//单位线回流时间
-
-        //加载Gr4j模型参数,
-        List<String[]> data = DataReader.colAndRowRead("./inputData.txt");
-        List<Double> P = new ArrayList<>();
-        List<Double> E = new ArrayList<>();
-        List<Double> Qobs = new ArrayList<>();
-
-        //将数据存到集合中
-        for (String[] datum : data) {
-            P.add(Double.parseDouble(datum[0]));
-            E.add(Double.parseDouble(datum[1]));
-            Qobs.add(Double.parseDouble(datum[2]));
-        }
-
-        int nStep = data.size();//观测数据的长度
+    public double[][] getPnAndEn(int nStep,List<Double> P, List<Double> E){
+//        //加载gr4j模型的状态变量和流域大小
+//        List<Double> otherParas = DataReader.colRead("./others.txt");
+//        double area = otherParas.get(0);//测试流域面积的大小（km2）
+//        double upperTankRadio = otherParas.get(1);//上层产流水库初始填充
+//        double lowerTankRadio = otherParas.get(2);//下层产流水库初始填充
+//
+//        //gr4j_Parameter参数读入
+//        List<Double> para = DataReader.colRead("./GR4J_Parameter.txt");
+//        double x1 = para.get(0);//产流水库容量
+//        double x2 = para.get(1);//地下水交换系数
+//        double x3 = para.get(2);//流域水库容量
+//        double x4 = para.get(3);//单位线回流时间
+//
+//        //加载Gr4j模型参数,
+//        List<String[]> data = DataReader.colAndRowRead("./inputData.txt");
+//        List<Double> P = new ArrayList<>();
+//        List<Double> E = new ArrayList<>();
+//        List<Double> Qobs = new ArrayList<>();
+//
+//        //将数据存到集合中
+//        for (String[] datum : data) {
+//            P.add(Double.parseDouble(datum[0]));
+//            E.add(Double.parseDouble(datum[1]));
+//            Qobs.add(Double.parseDouble(datum[2]));
+//        }
+//
+//        int nStep = data.size();//观测数据的长度
 
         double[] Pn = new double[nStep];//存储有效降雨量
         double[] En = new double[nStep];//存储剩余的蒸发能力
@@ -67,19 +66,39 @@ public class Calculate {
         return PnAndEn;
     }
 
+    /**
+     * @param Pn 净降雨量
+     * @param En 净蒸发量
+     * @param x1 产流水库容量
+     * @param S  产流水库逐日水量
+     * @return
+     */
+    public double CalPs(double Pn, double En, double x1, double S) {
+        double Ps;
+        if(En == 0){
+            Ps = x1 * (1 - Math.pow((S / x1),2)) * Math.tanh(Pn / x1) / (1 + S / x1 * Math.tanh(Pn / x1));
+        }else {
+            Ps = 0;
+        }
+        return Ps;
+    }
 
-    public void production_Store(double[][] PnAndEn,double S,double X1){
-        //获取Pn和En
-        double[] Pn = null;
-        double[] En = null;
-        for (int i = 0; i < PnAndEn[0].length; i++) {
-            Pn[i] = PnAndEn[0][i];
+
+    /**
+     * @param Pn 净降雨量
+     * @param En 净蒸发量
+     * @param x1 产流水库容量
+     * @param S  产流水库逐日水量
+     * @return
+     */
+    public double CalEs(double Pn, double En, double x1, double S) {
+        double Es;
+        if(Pn == 0){
+            Es = (S * (2 - (S / x1)) * Math.tanh(En / x1)) / (1 + (1 - S / x1) * Math.tanh(En / x1));
+        }else{
+            Es = 0;
         }
-        for (int i = 0; i < PnAndEn[1].length; i++) {
-            En[i] = PnAndEn[1][i];
-        }
-        double Ps;//产流水库增加水量
-        double Es;//产流水库消耗水量
+        return Es;
     }
     
     /***
@@ -126,6 +145,12 @@ public class Calculate {
      */
     public double calQr(double tempR, double x3){
         return tempR * (1 - Math.pow(1 + Math.pow(tempR / x3, 4), -0.25));
+    }
+
+    public double calPs(){
+        if(en = 0){
+            return 0
+        }
     }
 
 
