565c965b066b3402dfd661c752b9ae8a16767bd7
Marchino13 <1261302911@qq.com>
2024-06-15 23:41:17 +0800

    simulate初始化

---
+++ gr4j/src/main/java/com/sw/unit/simulateGr4j.java
@@ -0,0 +1,19 @@
+package com.sw.unit;
+
+/**
+ * @author Marchino
+ * @date 2024/6/15 23:37
+ * @description gr4j模型的模拟
+ */
+
+public class simulateGr4j {
+    public static double[] Gr4jSimulate(int dataLength, double x1, double x2, double x3, double x4,
+                                        double upperTankRatio, double lowerTankRatio, int maxDayDelay,
+                                        double[] UH1, double[] UH2, double[] Pn, double[] En){
+
+        for (int i = 0; i < dataLength; i++) {
+
+        }
+        return null;
+    }
+}
